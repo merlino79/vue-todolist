86d45555905e875ced71ac5bc9f621aed81c3b39
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-06 14:55:05 +0200

    stampa lista

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,6 +2,30 @@ const app = new Vue({
     el: '#app',
     data: {
         alertActive: false,
+        todos: [{
+                todo: 'sudiare',
+                checked: false
+            },
+            {
+                todo: 'divertirmi con vue Js',
+                checked: true
+            },
+            {
+                todo: 'studiare il codice e adattarlo',
+                checked: false
+            },
+            {
+                todo: 'disegnare a matita e finirlo in digitale',
+                checked: false
+            },
+
+        ]
+
+
+
+
+
+
 
 
     },
@@ -9,6 +33,6 @@ const app = new Vue({
 
 
     }
-})
+});
 
-console.log(app);
+//console.log(app);
